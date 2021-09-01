752a55a2f093703f3048ca91a7511362b6cde3ff
jose <jose.paternina.a@gmail.com>
2021-09-01 09:20:10 -0500

    Initial commit

---
+++ src/main/java/com/practice/roversimmulation/RoversimmulationApplication.java
@@ -0,0 +1,13 @@
+package com.practice.roversimmulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoversimmulationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoversimmulationApplication.class, args);
+	}
+
+}
